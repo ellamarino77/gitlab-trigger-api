2828d7585728575f7e4f405b456e77e549fd77d8
Ella Marino <marino77@students.rowan.edu>
2025-04-21 18:25:43 -0400

    fix

--- server.js
+++ server.js
@@ -2,19 +2,17 @@
 import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
-import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
 
 app.post("/trigger", async (req, res) => {
   const GITLAB_TOKEN = process.env.GITLAB_TRIGGER_TOKEN;
-  const GITLAB_PROJECT = "teamaxolotls%2Fdevsecopspipeline";
-  const REF = "main";
+  const GITLAB_PROJECT = process.env.GITLAB_PROJECT_PATH;
+  const REF = process.env.GITLAB_REF || "main";
 
   try {
     const response = await fetch(`https://gitlab.com/api/v4/projects/${GITLAB_PROJECT}/trigger/pipeline`, {
