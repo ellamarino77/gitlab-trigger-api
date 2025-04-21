a3c5c0050324101ea6fa6f17a94042b7ed3a0ea0
Ella Marino <marino77@students.rowan.edu>
2025-04-21 18:10:09 -0400

    change server.js

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/trigger", async (req, res) => {
